225b4d96bce3ada0847fcbec832ef00692a6bb1e
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-02-16 22:35:20 +0100

    Updated config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int instant = 0;                     /* -n  option; if 1, dmenu ends immediately on a distinct match */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"terminus:size=8"
+	"misc termsyn:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
