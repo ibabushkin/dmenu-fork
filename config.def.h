70e5b68cac658c3ee21fa872b7724aca84692b3a
Inokentiy Babushkin <twk@twki.de>
2017-05-03 22:27:32 +0200

    Updated colors

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#20201d", "#fefbec" },
-	[SchemeSel] = { "#20201d", "#6684e1" },
+	[SchemeSel] = { "#20201d", "#eeeeee" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
